eac1cb92793dc4091ef7423ad2e65cc01da8c336
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-08-25 10:44:26 +0200

    chore: setup basic server.js with test route

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// MIDDLEWARE
+app.use(express.json());
+
+// ROUTE
+app.get("/", (req, res) => {
+    res.send("Server är ansluten!");
+});
+
+// MONGODB-ANSLUTNING
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB ansluten");
+        app.listen(PORT, () => console.log(`Server körs på port ${PORT}`));
+    })
+    .catch((err) => console.error(err));
